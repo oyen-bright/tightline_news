9c6d99157082d952ff10c1f2a3109aaa9fa8d03e
oyen-bright <oyenbrihight@gmail.com>
2026-04-02 01:57:58 +0100

    Update dependencies in pubspec.yaml, implement dependency injection with GetIt, and configure Dio client for network requests

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:tightline_news/app/app.dart';
 import 'package:tightline_news/core/config/app_config.dart';
+import 'package:tightline_news/core/di/injection.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppConfig.load();
+  await configureDependencies();
   runApp(const App());
 }
