568f8003c247302352e120b5d51e28211bcf6fea
oyen-bright <oyenbrihight@gmail.com>
2026-04-02 01:09:54 +0100

    Add app configuration files and update main.dart for environment setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tightline_news/app/app.dart';
+import 'package:tightline_news/core/config/app_config.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppConfig.load();
   runApp(const App());
 }
